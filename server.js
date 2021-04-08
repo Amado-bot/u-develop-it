88cf2d3ed8165f8d5740e8a5082b052eceef713a
unknown <amadocardenas0@gmail.com>
2021-04-07 19:40:20 -0600

    proof of accomplishment

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ app.use(express.urlencoded({
 }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
